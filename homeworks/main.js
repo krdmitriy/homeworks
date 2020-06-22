7b7c690c303e28fea854e3b64f8fde4d09d73794
krdmitriy <67235029+krdmitriy@users.noreply.github.com>
2020-06-22 16:42:31 +0300

    Add files via upload

---
+++ homeworks/main.js
@@ -0,0 +1,18 @@
+//Задание 1
+
+let algebra = 4;
+let geography = 5;
+let physics = 2; 
+let mean = (algebra + geography + physics) / 3;
+
+console.log(mean);
+
+
+console.log();
+//Задание 2 
+let name = 'Дмитрий';
+let message = `Кто тут ${name}?`; 
+
+console.log(message);
+
+console.log();
